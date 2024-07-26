bb73d2680f8d9e5c21e316afd38628125ceec189
carlomanso <carlosmanzasola@gmail.com>
2024-07-26 15:30:07 +0200

    Terminada implementación por programación dinámica. Corrección de bugs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 use std::mem::size_of;
 use std::time::Instant;
-use simple_matrix::Matrix;
-const tam_columnas: usize = 2;
-const tam_filas: usize = 7;
+
+const TAM_FILAS: usize = 7;
+const TAM_COLUMNAS: usize = 2;
 fn main() {
     std::thread::Builder::new()
         .stack_size(size_of::<f64>() * 2_000_000_000) // El stack es lento para hacer operaciones y sospecho que se puede recalcular mediante programación dinamica
         .spawn(|| {
-            for m in 0..tam_filas {
+            println!("\n Por definición:");
+            for m in 0..TAM_FILAS-2 {
                 print!("\nm={m}   ");
-                for n in 0..tam_columnas {
+                for n in 0..TAM_COLUMNAS {
                     let inicio = Instant::now();
                     let res = ackermann_recursivo_def(m as u8, n as u128);
                     let dur = inicio.elapsed();
                     print!("Resultado: {}, {:?}", res, dur);
-                    if n < tam_columnas {
+                    if n < TAM_COLUMNAS-1 {
+                        print!("  ||  ")
+                    }
+                }
+                println!();
+            }
+            println!("\n Por programación dinámica:");// Considerar hacer cada uno de los apartados hilos distintos para que se hagan en paralelo
+            let mut matriz: [[u128; TAM_COLUMNAS]; TAM_FILAS] = [[0; TAM_COLUMNAS]; TAM_FILAS];
+            for m in 0..TAM_FILAS {
+                print!("\nm={m}   ");
+                for n in 0..TAM_COLUMNAS {
+                    let inicio = Instant::now();
+                    let res = ackermann_dinamico(m as u8, n as u128, &mut matriz);
+                    let dur = inicio.elapsed();
+                    print!("Resultado: {}, {:?}", res, dur);
+                    if n < TAM_COLUMNAS-1 {
                         print!("  ||  ")
                     }
                 }
@@ -27,25 +43,38 @@ fn main() {
 }
 
 fn ackermann_recursivo_def(m: u8, n: u128) -> u128 {
-    if m == 0 {
+    /* La operación del match es más lenta que la versión con if statements
+    let resultado:u128 =match (m,n) {
+        (0,_) => n+1,
+        (_,0) => ackermann_recursivo_def(m-1,1),
+        _ => ackermann_recursivo_def(m-1,ackermann_recursivo_def(m,n-1))
+    };
+    return resultado;
+    */
+    return if m == 0 {
         return n + 1;
+    } else if m > 0 && n == 0 {
+        ackermann_recursivo_def(m - 1, 1)
+    } else {
+        ackermann_recursivo_def(m - 1, ackermann_recursivo_def(m, n - 1))
     }
-    if m > 0 && n == 0 {
-        return ackermann_recursivo_def(m - 1, 1);
-    }
-    if m > 0 && n > 0 {
-        return ackermann_recursivo_def(m - 1, ackermann_recursivo_def(m, n - 1));
-    }
-    return 0;
 }
-fn ackermann_dinamico(m: u8, n: u128) -> u128 {
-    let matriz:Matrix<u128>=Matrix::new(tam_filas,tam_columnas);
-    for i in 1..tam_filas+1{
-        for j in 1..tam_columnas+1{
-            if matriz.get(i,j).unwrap() != &0 {//Continuar por aqui
-                unimplemented!();
-            }
-        }
+fn ackermann_dinamico(m: u8, n: u128, matriz: &mut [[u128; TAM_COLUMNAS]; TAM_FILAS]) -> u128 {
+    if m as usize >= matriz.len() || n as usize >= matriz[0].len() {
+        print!("Error: ");
+        return 0; // Devuelve un valor predeterminado si los índices están fuera de los límites
     }
-    n //TODO Metodo por realizar
+    if matriz[m as usize][n as usize] != 0 { //Index out of bounds, probablemente debido a n, el indice 2 esta fuera de limites para la longitud 2
+        return matriz[m as usize][n as usize];
+    }
+    let resultado:u128;
+    if m == 0 {
+        resultado=n + 1;
+    } else if m > 0 && n == 0 {
+        resultado=ackermann_recursivo_def(m - 1, 1);
+    } else {
+        resultado=ackermann_recursivo_def(m - 1, ackermann_recursivo_def(m, n - 1));
+    }
+    matriz[m as usize][n as usize]=resultado;
+    return resultado
 }
